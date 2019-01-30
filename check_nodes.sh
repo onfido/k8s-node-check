c4352c5e9ac95f8697a1086b46f50fcb64b53473
Mikko Caldara <mikko.caldara@onfido.com>
2019-01-30 15:50:56 +0000

    it's a variable
    Treat it as such

--- check_nodes.sh
+++ check_nodes.sh
@@ -7,13 +7,13 @@ while true; do
     notReadyNodes=$(kubectl get nodes -o json | jq -r '.items[] | select ( .status.conditions[] | select( .type=="Ready" and .status=="False")) | .metadata.name')
 
     # for each node in status=Ready, send:
-    for node in readyNodes; do
+    for node in $readyNodes; do
         echo "kubernetes.node_check:1|g|#status:Ready,node:$node" | nc -w 1 -u $DOGSTATSD_HOST_IP 8125
         echo "kubernetes.node_check:0|g|#status:NotReady,node:$node" | nc -w 1 -u $DOGSTATSD_HOST_IP 8125
     done
 
     # for each node in status=NotReady, send:
-    for node in notReadyNodes; do
+    for node in $notReadyNodes; do
         echo "kubernetes.node_check:1|g|#status:NotReady,node:$node" | nc -w 1 -u $DOGSTATSD_HOST_IP 8125
         echo "kubernetes.node_check:0|g|#status:Ready,node:$node" | nc -w 1 -u $DOGSTATSD_HOST_IP 8125
     done
